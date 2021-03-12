75a2192ada7b8c28381b2ac44b824bdc3303b90a
jacace <javierandrescaceres@gmail.com>
2021-03-12 11:01:42 +0000

    Added put and get tests

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #To run vault in dev:
 docker-compose -f docker-compose.dev.yml up --build -d
 
-#verify teh server is running (you can also open the ui: http://0.0.0.0:8201/ui/vault/auth?with=token)
+#Verify the server is running (you can also open the ui: http://0.0.0.0:8201/ui/vault/auth?with=token)
 export VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
 export VAULT_ADDR='http://0.0.0.0:8201'
-
-
 ./vault status
+
+#Enable a new path (aka secrets engine)
+./vault secrets enable -path=testpath kv
+./vault secrets list
+#To create a secret
+./vault kv put testpath/namesecret target=valuesecret
+#To read a secret
+./vault kv get testpath/namesecret
