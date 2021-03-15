ad5fd16ec0e10bd181f6798715e821807c2a8e47
jacace <javierandrescaceres@gmail.com>
2021-03-15 18:32:18 +0000

    Refactored main script

--- test.sh
+++ test.sh
@@ -5,17 +5,23 @@ docker-compose -f docker-compose.dev.yml up --build -d
 export VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
 export VAULT_ADDR='http://0.0.0.0:8201'
 ./vault status
-
-#Enable a new path (aka secrets engine, aka virtual filesystem)
-./vault secrets enable -path=testpath kv
 ./vault secrets list
-#To create a secret
-./vault kv put testpath/namesecret target=valuesecret
-#To read a secret
-./vault kv get testpath/namesecret
 
-#Enable AWS secrets engine
+#Test with Key Vaule secrets engine
+#./vault secrets enable -path=testpath kv
+#./vault kv put testpath/namesecret target=valuesecret
+#./vault kv get testpath/namesecret
+
+#Creds for Terraform
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+
+#Enable AWS secrets engine (aka secrets engine, aka virtual filesystem, aka path) 
+#Using the TF IAM vault user (not root for TF above)
 ./vault secrets enable -path=awsvfs aws
-export AWS_ACCESS_KEY_ID=<aws_access_key_id>
-export AWS_SECRET_ACCESS_KEY=<aws_secret_key>
+export VAULT_AWS_ACCESS_KEY_ID=
+export VAULTAWS_SECRET_ACCESS_KEY=
 
+./vault write awsvfs/config/root access_key=$VAULT_AWS_ACCESS_KEY_ID secret_key=$VAULTAWS_SECRET_ACCESS_KEY region=us-east-1
+./vault write awsvfs/roles/vault_kinesis_ro_role role_arns=arn:aws:iam::<ACC_NUM>:role/kinesis_ro_role credential_type=assumed_role
+./vault read awsvfs/creds/vault_kinesis_ro_role
